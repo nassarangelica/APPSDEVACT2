cc7535398e37db440e769e01b8959be3f3461dd7
Angelica Nassar <https://github.com/nassarangelica>
2025-08-17 22:46:21 +0800

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import ProductCard from './component/ProductCard';
-import Ads from './component/ads';
-import Sidebar from './component/sidebar';
+import Sidebar from './component/Sidebar';
 
 function App() {
 
@@ -10,70 +9,136 @@ function App() {
   const product = [
     {
       id: 1,
-      image: "./1.jpg",
-      name: "Logitech G502 X Plus",
+      image: "./1.png",
+      name: "Taylor Swift",
       price: "$140",
-      description: "The G502 X Plus is the latest in a long line of G502 winners. The wireless pointer packs a seriously smooth sensor and a wad of extra buttons",
+      description: "Taylor Swift’s self-titled debut studio album, released on October 24, 2006, marked her breakthrough into the country music scene. Written during her teenage years, the album captures themes of love, heartbreak, friendship, and self-discovery, reflecting her personal experiences. ",
     },
     {
       id: 2,
-      image: "./2.jpg",
-      name: "Logitech G Pro X",
+      image: "./2.png",
+      name: "Fearless (Taylor's Version)",
       price: "$90",
-      description: "Logitech G Pro X Superlight 2 DEX, With its improved form factor, fantastic clicks, and speedy sensor, this isn't just the best Logitech gaming mouse, it's the best pointer I've used so far"
+      description: "Fearless is the second studio album by American singer-songwriter Taylor Swift, released on November 11, 2008, in North America through Big Machine Records and later internationally in 2009. "
     },
     {
       id: 3,
-      image: "./3.jpg",
-      name: "Logitech G203",
+      image: "./3.png",
+      name: "Speak Now (Taylor's Version)",
       price: "$160",
-      description: "Logitech G203 Wired Gaming Mouse - 8,000 DPI - LIGHTSYNC RGB - 6 Programmable Buttons"
+      description: "Speak Now is the third studio album by American singer-songwriter Taylor Swift, released on October 25, 2010, through Big Machine Records. Written entirely by Swift during her 2009–2010 tour, the album reflects her transition from adolescence to adulthood with deeply personal, confessional songwriting."
     },
     {
       id: 4,
-      image: "./4.jpg",
-      name: "Gigabyte M32UC",
+      image: "./4.png",
+      name: "Red (Taylor's Version)",
       price: "$170",
-      description: "The Gigabyte M32UC packs in a 4K resolution and a 144Hz refresh rate all for an all-star price point which few of its competitors can claim."
+      description: "Red, released on October 22, 2012, is Taylor Swift’s fourth studio album and a pivotal moment in her career. Marketed as her final country album, it bridges her country roots with a bold pop crossover. "
     },
     {
       id: 5,
-      image: "./5.jpg",
-      name: "Asus TUF Gaming VG289Q",
+      image: "./5.png",
+      name: "1989 (Taylor's Version)",
       price: "$450",
-      description: "The Asus TUF Gaming VG289Q is an outstanding value for the asking price. You get a beautiful 28-inch 4K display with all kinds of options for adjustments at an entry-level price range."
+      description: "Taylor Swift’s 1989, released on October 27, 2014, marked a pivotal shift in her career as she transitioned from country to full-fledged pop. Inspired by 1980s synth-pop, the album featured polished production from Max Martin, Shellback, Jack Antonoff, and others, showcasing lush synthesizers, drum machines, and layered vocals."
     },
     {
       id: 6,
-      image: "./6.jpg",
-      name: "Samsung Odyssey OLED G9",
-      price: "$500",
-      description: "The Samsung Odyssey OLED G9 doesn't come cheap, but it features a superfast 240Hz refresh rate, and an incredible 32:9 aspect ratio making the most of its extended 1440p resolution."
+      image: "./6.png",
+      name: "Reputation",
+      price: "$140",
+      description: "Taylor Swift’s Reputation is her sixth studio album, released on November 10, 2017, through Big Machine Records. Conceived amid intense media scrutiny and public feuds, the album marked a reinvention of Swift’s image from “America’s Sweetheart” to a darker, more defiant persona."
+    },
+    {
+      id: 7,
+      image: "./7.png",
+      name: "Lover",
+      price: "$140",
+      description: "Lover is her seventh studio album, released on August 23, 2019, through Republic Records. Marking her first project after parting ways with Big Machine Records, the album symbolized creative freedom and a softer, romantic reinvention following the darker Reputation era. ",
+    },
+    {
+      id: 8,
+      image: "./8.png",
+      name: "Folklore",
+      price: "$140",
+      description: "Folklore is Taylor Swift’s eighth studio album, released on July 24, 2020, through Republic Records as a surprise during the COVID-19 pandemic. Conceived in quarantine, the album marked a major shift in Swift’s artistry, embracing fictional storytelling, character-driven narratives, and a cottagecore-inspired aesthetic.",
+    },
+    {
+      id: 9,
+      image: "./9.png",
+      name: "Evermore",
+      price: "$140",
+      description: "Evermore is Taylor Swift’s ninth studio album, released as a surprise on December 11, 2020, through Republic Records. Conceived as the “sister record” to Folklore, it continues Swift’s exploration of indie folk, folk-pop, and alternative rock, created primarily with Aaron Dessner at Long Pond Studio during the COVID-19 pandemic.",
+    },
+    {
+      id: 10,
+      image: "./10.png",
+      name: "Midnights",
+      price: "$150",
+      description: "Midnights is the tenth studio album by American singer-songwriter Taylor Swift, released on October 21, 2022, through Republic Records. Conceived as a concept album inspired by her sleepless nights, it explores themes of regret, self-criticism, infatuation, heartbreak, and revenge.",
+    },
+    {
+      id: 11,
+      image: "./11.png",
+      name: "The Tortured Poets Department",
+      price: "$160",
+      description: "The Tortured Poets Department is Taylor Swift’s eleventh studio album, released on April 19, 2024, through Republic Records. Crafted alongside Jack Antonoff and Aaron Dessner, Swift described it as her “lifeline” album, born from the intense media scrutiny during her Eras Tour. ",
+    },
+    {
+      id: 12,
+      image: "./12.png",
+      name: "The Life of a Show Girl",
+      price: "$800",
+      description: "The Life of a Showgirl, is set for release on October 3, 2025, through Republic Records. Conceived during the European leg of her record-breaking Eras Tour, the album reflects Swift’s experiences behind the scenes of her life as an entertainer. ",
     }
   ];
 
-  const prodData = product.filter((prod) => {
-    return prod.name.toLowerCase().includes(Search.toLocaleLowerCase()) || prod.price.toLowerCase().includes(Search.toLocaleLowerCase())
-  })
-
+ const prodData = product.filter((prod) => {
+    return prod.name.toLowerCase().includes(Search.toLowerCase()) || 
+           prod.price.toLowerCase().includes(Search.toLowerCase())
+  });
 
   return (
-    <div className='w-auto h-auto flex items-center justify-center'>
-      <div className='absolute left-15 top-50 fixed'>
-            <Sidebar Search={Search} setSearch={setSearch}/>
-          </div>
-      <div className='flex flex-wrap gap-8 w-1/2 justify-center p-0 m-10'>
-        {prodData.map((proditem) => (
-          <ProductCard key={proditem.id} image={proditem.image} name={proditem.name} price={proditem.price} description={proditem.description} onBuy={() => console.log(`Thank you for buying: ${product.name}`)} />
-        ))}
-      </div>
-          <div className='absolute right-5 fixed top-10'>
-            <Ads/>
-          </div>
-          
-    </div>
+    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('./bg.png')" }}>
+     
+      {/* Header sa albums */}
+      <header className="w-full bg-black/70 text-white px-8 py-4 flex flex-col md:flex-row md:items-center md:justify-between shadow-md gap-4">
+        <h1 className="text-2xl font-bold">Albums</h1>
+        
+        {/* New Find Products Search Bar */}
+        <div className="flex items-center bg-white rounded-full px-4 py-2 shadow-md w-full md:w-96">
+          <input
+            type="text"
+            placeholder="Find Products..."
+            value={Search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full text-black outline-none bg-transparent"
+          />
+        </div>
+      </header>
 
+      {/* Product Grid */}
+      <main className="p-10 flex justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-7xl">
+          {prodData.map((proditem) => (
+            <ProductCard 
+              key={proditem.id} 
+              image={proditem.image} 
+              name={proditem.name} 
+              price={proditem.price} 
+              description={proditem.description} 
+              onBuy={() => console.log(`Thank you for buying: ${proditem.name}`)} 
+            />
+          ))}
+        </div>
+      </main>
+
+      {/* Footer */}
+    <footer className="bg-black/80 text-white text-center py-4">
+      <p>&copy; {new Date().getFullYear()} My Album Store. Nassar Project Appsdev 2025.</p>
+    </footer>
+  </div>
   )
 }
 
-export default App
+export default App;
--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { HiOutlineShoppingCart } from "react-icons/hi2";
+import React from "react";
 
-function ProductCard({image, name, price, description, onBuy}) {
+function ProductCard({ image, name, price, description, onBuy }) {
   return (
-    <div className='sm:w-50 md:w-70 h-85 flex justify-center items-center text-center flex-col text-balance bg-gray-200 rounded-xl gap-3 cardhover'>
-      <img src={image} alt={name} className='sm:w-15 md:w-30 h-30 rounded-xl object-contain mt-10' />
-      <h2 className='text-lg font-semibold text-gray-900'>{name}</h2>
-      <p className='text-xl font-bold text-green-700'>{price}</p>
-      <p className='leading-snug m-0 p-0 text-xs text-gray-600'>{description}</p>
-      <button onClick={onBuy} className='mb-2 w-40 bg-blue-900 text-white py-2 px-2 rounded-lg bt-hover hover:text-gray-600 cursor-pointer  font-semibold flex items-center justify-center gap-2'><HiOutlineShoppingCart />BUY NOW</button>
+    <div className="bg-white rounded-2xl shadow-lg flex flex-col overflow-hidden w-full h-[450px]">
+      {/* Album Image */}
+      <div className="h-48 w-full flex items-center justify-center bg-gray-100">
+        <img src={image} alt={name} className="h-full object-contain" />
+      </div>
+
+      {/* Card Content */}
+      <div className="p-4 flex flex-col flex-grow">
+        <h2 className="text-lg font-bold mb-2 text-center">{name}</h2>
+        <p className="text-green-700 font-semibold text-center mb-2">{price}</p>
+        <p className="text-gray-600 text-sm flex-grow line-clamp-4">{description}</p>
+        <button 
+          onClick={onBuy} 
+          className="mt-4 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
+        >BUY NOW
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductCard
+export default ProductCard;
